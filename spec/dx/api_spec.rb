02145738acce846bca3346e7ee3068ced394e84d
Kurt Mueller <kurt.mueller@osumc.edu>
2020-08-03 16:32:56 -0400

    Fix specs

--- spec/dx/api_spec.rb
+++ spec/dx/api_spec.rb
@@ -1,9 +1,5 @@
-RSpec.describe Dx::Api do
+RSpec.describe DX::Api do
   it "has a version number" do
-    expect(Dx::Api::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+    expect(DX::Api::VERSION).to_not be nil
   end
 end
