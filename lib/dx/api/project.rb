57d51a3f3980db3ad63c5816dbf19d9c8f82ccf0
Kurt Mueller <kurt.mueller@osumc.edu>
2020-08-04 08:41:30 -0400

    Create new projects

--- lib/dx/api/project.rb
+++ lib/dx/api/project.rb
@@ -3,12 +3,19 @@ require 'json'
 
 module DX
   module Api
+    # Access the DNAnexus project api.
+    # https://documentation.dnanexus.com/developer/api/data-containers/projects
     module Project
       # Describe the specified project
       # https://documentation.dnanexus.com/developer/api/data-containers/projects#api-method-project-xxxx-describe
       #
-      # @param api_token [String] Your DNAnexus api token.
-      # @param project_id [String] The full id of the project.
+      # DX::Api::Project.describe(
+      #   api_token: "api_token",
+      #   project_id:"project-1234"
+      # )
+      #
+      # @param api_token [String] Your DNAnexus api token
+      # @param project_id [String] The full id of the project
       # @return [Hash] A hash that contains the response code and json-parsed body
       def self.describe(api_token:, project_id:)
         uri = [DX::Api::HOST_NAME, project_id, 'describe'].join('/').then { |url| URI(url) }
@@ -26,7 +33,38 @@ module DX
         end
       end
 
-      def self.create(api_token:); end
+      # Create a new project
+      # https://documentation.dnanexus.com/developer/api/data-containers/projects#api-method-project-new
+      #
+      # DX::Api::Project.create(
+      #   api_token: "api_token",
+      #   name: "Project name",
+      #   summary: "Project summary"
+      # )
+      #
+      # @param api_token [String] Your DNAnexus api token
+      # @param name [String] The project name
+      # @param summary [String] The project summary
+      # @return [Hash] A hash that contains the response code and the id of created project
+      def self.create(api_token:, name:, summary:)
+        uri = [DX::Api::HOST_NAME, 'project', 'new'].join('/').then { |url| URI(url) }
+
+        Net::HTTP.post(
+          uri,
+          {
+            name: name,
+            summary: summary,
+            description: summary
+          }.to_json,
+          'Content-Type' => 'application/json',
+          'Authorization' => "Bearer #{api_token}"
+        ).then do |resp|
+          {
+            code: resp.code.to_i,
+            body: resp.body.then(&JSON.method(:parse))
+          }
+        end
+      end
     end
   end
 end
