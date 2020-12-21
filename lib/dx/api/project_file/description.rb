86d7664a44c748cb7758c04537f427707996e3e2
Kurt Mueller <kurt.mueller@osumc.edu>
2020-12-21 14:14:58 -0500

    Add media to project file description

--- lib/dx/api/project_file/description.rb
+++ lib/dx/api/project_file/description.rb
@@ -14,18 +14,30 @@ module DX
             name: describe.fetch('name'),
             state: describe.fetch('state'),
             folder: describe.fetch('folder'),
-            archival_state: describe.fetch('archivalState')
+            archival_state: describe.fetch('archivalState'),
+            media: describe.fetch('media')
           )
         end
 
-        attr_reader :project_id, :id, :name, :state, :folder, :archival_state
+        attr_reader :project_id, :id, :name, :state, :folder, :archival_state, :media
 
-        def initialize(project_id:, id:, name:, state:, folder:, archival_state:)
+        # A project file description.
+        # https://documentation.dnanexus.com/developer/api/introduction-to-data-object-classes/files#api-method-file-xxxx-describe
+        #
+        # @param project_id [String] The file's project id
+        # @param id [String] The file id
+        # @param name [String] The file name
+        # @param folder [String] The full path to the folder containing the file
+        # @param state [String] The value "open", "closing", or "closed"
+        # @param archival_state [String] The archival state of the file
+        # @param media [String] The Internet Media Type of the file
+        def initialize(project_id:, id:, name:, folder:, state:, archival_state:, media:)
           @project_id = project_id
           @id = id
           @name = name
           @folder = folder
           @archival_state = archival_state
+          @media = media
         end
       end
     end
